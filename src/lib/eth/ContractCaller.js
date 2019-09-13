c3e5b56b954dfe19fc2d0d50625da93628f4732b
Ivan Levitsky <info@pointpay.io>
2019-09-13 21:24:06 +0300

    ETH: status is status now

--- src/lib/eth/ContractCaller.js
+++ src/lib/eth/ContractCaller.js
@@ -23,8 +23,8 @@ class ContractCaller {
 
     async callViewAsync(methodName, methodArguments) {
         new Promise(then => {
-            this.callView(methodName, methodArguments, (s, v) => {
-                then(s,v);
+            this.callView(methodName, methodArguments, (status, v) => {
+                then(status,v);
             })
         })
     }
