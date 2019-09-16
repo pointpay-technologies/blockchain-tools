573a437b907fa49f80f14594853e1f4fa10d8ce7
Ivan Levitsky <info@pointpay.io>
2019-09-16 21:14:23 +0300

    ETH: letters separated by digits now

--- src/lib/util/Util.js
+++ src/lib/util/Util.js
@@ -2,13 +2,14 @@ class Util {
     randomString(length = 10, chars = null) {
         let rStr = '';
         if (!chars)
-            chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            chars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         let charsLen = chars.length;
         for (let i = 0; i < charsLen; i++) {
             rStr += chars.charAt(Math.floor(Math.random() * charsLen));
         }
         return rStr;
     }
+
 }
 
 module.exports = Util;
