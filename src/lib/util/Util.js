4f33c94abe4b9499661b6dddcb3182841a22bb3f
Ivan Levitsky <info@pointpay.io>
2019-09-19 23:32:44 +0300

    Utils: renamed var

--- src/lib/util/Util.js
+++ src/lib/util/Util.js
@@ -1,13 +1,13 @@
 class Util {
     randomString(length = 10, chars = null) {
-        let rStr = '';
+        let ret = '';
         if (!chars)
             chars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         let charsLen = chars.length;
         for (let i = 0; i < charsLen; i++) {
-            rStr += chars.charAt(Math.floor(Math.random() * charsLen));
+            ret += chars.charAt(Math.floor(Math.random() * charsLen));
         }
-        return rStr;
+        return ret;
     }
 }
 
