c2284f6765f812b9481503f779b501d7fb015839
pointpay-technologies <46765255+pointpay-technologies@users.noreply.github.com>
2020-01-23 15:40:51 +0300

    Update Util.js
    Added calculation function

--- src/lib/util/Util.js
+++ src/lib/util/Util.js
@@ -31,5 +31,60 @@ export default class Util {
     filterRange(arr, a, b) {
     return arr.filter(item => item>=a && item<=b);
 }
-    
+    calculate(give: boolean) {
+            for (let meth in method) {
+                for (let keys in paywayMethod) {
+                    let arr = paywayMethod[keys];
+                    for (let payMethod of arr) {
+                        for (let crypto of cryptoCurrency) {
+                            let params: any = {
+                                direction: meth,
+                                payway: payMethod,
+                            };
+                            let isCrypto: boolean;
+                            if (meth === "buy") {
+                                params.giveCurrency = keys;
+                                params.receiveCurrency = crypto;
+                                isCrypto = false;
+                            } else {
+                                params.receiveCurrency = keys;
+                                params.giveCurrency = crypto;
+                                isCrypto = true
+                            }
+
+                            if (give) {
+                                params.giveOrReceive = 'giveAmount';
+                            } else {
+                                params.giveOrReceive = 'receiveAmount';
+                            }
+
+                            // todo: set different amounts: 0, normal and too large:
+                            for (let diffAmount in setAmount) {
+                                //if there is property in setAmount
+                                if (isCrypto && utils.getNested(setAmount, diffAmount, "crypto", [crypto])) {
+                                    console.log('first: ' + setAmount[diffAmount]["crypto"][crypto]);
+                                    params.amount = setAmount[diffAmount]["crypto"][crypto];
+                                } else if (isCrypto && utils.getNested(setAmount, diffAmount, "crypto")) {
+                                    console.log('second: ' + setAmount[diffAmount]["crypto"]);
+                                    params.amount = setAmount[diffAmount]["crypto"];
+                                } else if (!isCrypto && utils.getNested(setAmount, diffAmount, "fiat", keys)) {
+                                    console.log('third' + setAmount[diffAmount]["fiat"][keys]);
+                                    params.amount = setAmount[diffAmount]["fiat"][keys];
+                                } else if (!isCrypto && utils.getNested(setAmount, diffAmount, "fiat")) {
+                                    console.log('forth' + setAmount[diffAmount]["crypto"]);
+                                    params.amount = setAmount[diffAmount]["fiat"];
+                                } else {
+                                    console.log('else' + setAmount[diffAmount]);
+                                    params.amount = setAmount[diffAmount];
+                                }
+                            }
+                            it(`test for ${JSON.stringify(params)}`, () => {
+                                exchangeAPI.calculate(params);
+                            });
+
+                        }
+                    }
+                }
+            }
+        }
 }
