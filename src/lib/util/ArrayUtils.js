ba2474646047024a1c844f91dd0825a78cfa35e7
pointpay-technologies <46765255+pointpay-technologies@users.noreply.github.com>
2020-02-18 09:51:00 +0300

    Added function for stake check

--- src/lib/util/ArrayUtils.js
+++ src/lib/util/ArrayUtils.js
@@ -6,5 +6,39 @@ export class CreateUtils extends Utils {
   getCryptos(arr, short) {
     return arr.map(item => item.short===short);
   }
+  checkStr(str) {
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      case '(':
+      case '{':
+      case '[':
+        arr.push(str[i]);
+        break;
+      case '}':
+        if (arr.length === 0 || arr[arr.length - 1] !== '{') {
+          return console.log(str.length);
+        } else {
+          arr.pop();
+        }
+        break;
+      case ']':
+        if (arr.length === 0 || arr[arr.length - 1] !== '[') {
+          return str.length;
+        } else {
+          arr.pop();
+        }
+        break;
+      case ')':
+        if (arr.length === 0 || arr[arr.length - 1] !== '(') {
+          return str.length;
+        } else {
+          arr.pop();
+        }
+        break;
+    }
+  }
+  return arr.length === 0 ? console.log('Success') : console.log(str.length);
+}
 }
 
