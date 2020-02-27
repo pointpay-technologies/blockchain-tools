8c52292305422eaa0b6a3e09c613b5e0b260bb9d
pointpay-technologies <46765255+pointpay-technologies@users.noreply.github.com>
2020-02-27 07:35:24 +0300

    Added erc-20 transfer

--- src/lib/eth/ERC20Caller.js
+++ src/lib/eth/ERC20Caller.js
@@ -1,6 +1,6 @@
 let abi = require('./ERC20Abi');
 let EthContractCaller = require('./EthContractCaller');
-
+const web3 = require('web3');
 class ERC20Caller {
     /**
      *
@@ -15,8 +15,27 @@ class ERC20Caller {
         this.toAddress = toAddress;
     }
 
-    async transfer() {
+    async transfer(contractAddress, value, data) {
+        let myContract = new web3.eth.Contract(abi, contractAddress);
+        let data = myContract.methods.transfer(toAddress, value).encodeABI();
+        let rawTx = {
+             "nonce": web3.utils.toHex(nonce),
+              "gasPrice": "0x3b9aca00",
+                "gasLimit": web3.utils.toHex(gasLimit),
+                "to": contractAddress,
+                 "value": value,
+                 "data": data,
+        }
+        const tx = new Tx(rawTx)
+        tx.sign(privateKey)
+        let serializedTx = "0x" + tx.serialize().toString('hex');
+        web3.eth.sendSignedTransaction(serializedTx).on('transactionHash', function (txHash) {
 
+        }).on('receipt', function (receipt) {
+        }).on('confirmation', function (confirmationNumber, receipt) {
+        }).on('error', function (error) {
+
+        });
     }
     
     async transferFrom() {
